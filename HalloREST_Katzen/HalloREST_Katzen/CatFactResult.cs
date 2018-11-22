a751ae27750ecae3bdd27cbeac7bd3181eac0b7f
André <andrerrr@outlook.de>
2018-11-22 16:07:04 +0100

    Json und Katzen

---
+++ HalloREST_Katzen/HalloREST_Katzen/CatFactResult.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HalloREST_Katzen
+{
+
+    public class CatFactResult
+    {
+        public All[] all { get; set; }
+        public object[] me { get; set; }
+    }
+
+    public class All
+    {
+        public string _id { get; set; }
+        public string text { get; set; }
+        public User user { get; set; }
+        public Upvote[] upvotes { get; set; }
+    }
+
+    public class User
+    {
+        public string _id { get; set; }
+        public Name name { get; set; }
+    }
+
+    public class Name
+    {
+        public string first { get; set; }
+        public string last { get; set; }
+    }
+
+    public class Upvote
+    {
+        public string user { get; set; }
+    }
+
+}
---
+++ HalloREST_Katzen/HalloREST_Katzen/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Windows;
+
+namespace HalloREST_Katzen
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void LoadJson(object sender, RoutedEventArgs e)
+        {
+            using (var wc = new WebClient())
+            {
+                wc.Encoding = Encoding.UTF8;
+                string json = await wc.DownloadStringTaskAsync("https://cat-fact.herokuapp.com/facts");
+                jsonTb.Text = json;
+
+                CatFactResult result = JsonConvert.DeserializeObject<CatFactResult>(json);
+                myGrid.ItemsSource = result.all.Where(x => x.text.Length > 5).OrderByDescending(x => x.upvotes.Length);
+            }//wc.Dispose()
+        }
+    }
+}
